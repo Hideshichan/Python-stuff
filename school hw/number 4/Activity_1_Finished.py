f6a6120b5c152b616ca6bf8b069ec9097eb2d62d
Hideshichan <hideshi.chan@outlook.com>
2024-10-03 19:26:24 +0100

    hi new hw just droppped

---
+++ school hw/number 4/Activity_1_Finished.py
@@ -0,0 +1,74 @@
+# ------------------------------------------------------------
+# Import libraries
+# ------------------------------------------------------------
+import random
+import math
+import time
+
+# ------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------
+MAX_COUNT = 100             # Max number of items
+PI = math.pi
+
+# ------------------------------------------------------------
+# Global variables
+# ------------------------------------------------------------
+myList = []                 # To be filled with real numbers
+mean = 0.0
+abovePi = 0
+
+# ------------------------------------------------------------
+# Subprograms
+# ------------------------------------------------------------
+def genRandomReals (pList, pCount):
+    number = 0.0
+
+    # Complete the code to generate pCount number of random
+    # real numbers (with decimals) and append each of them
+    # to pList
+    for i in range(pCount):
+        pList.append(random.random() * 10)
+
+# =====> Write the countAbovePi subprogram here.
+#        It takes a list as a parameter and returns an integer.
+def countAbovePi(pList):
+    numberabovepi = 0
+    for item in myList:
+        if item > PI:
+            numberabovepi += 1
+    return numberabovepi
+
+# =====> Write the findMean subprogram here.
+#        It takes a list as a parameter and returns a real number.
+def findMean(pList):
+    total = 0
+    for item in pList:
+        total += item
+    meantotal = total/len(myList)
+    return meantotal
+
+# ------------------------------------------------------------
+# Main program
+# ------------------------------------------------------------
+# =====> Add a message for thinking time and wait 1 second
+print("Waiting 1 second")
+time.sleep(1)
+genRandomReals (myList, MAX_COUNT)
+print (myList)
+
+# =====> Add a message for thinking time and wait 1 second
+print("Waiting 1 second")
+time.sleep(1)
+# =====> Call the countAbovePi subprogram and display result
+abovePi = countAbovePi(myList)
+print ("Above Pi: ", abovePi)
+
+# =====> Add a message for thinking time and wait 1 second
+print("Waiting 1 second")
+time.sleep(1)
+# =====> Call the findMean subprogram and display the result
+mean = findMean(myList)
+print ("Mean: ", mean)
+
+print ("Goodbye")
